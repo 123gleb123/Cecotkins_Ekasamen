c51b760ddbe75e3dbba5c76b1873a97cb4b93219
123gleb123 <gchechotkin@gmail.com>
2025-06-11 10:02:11 +0300

    pabeidzu taisit 3.uzd

--- src/uzd3.java
+++ src/uzd3.java
@@ -1,6 +1,36 @@
+import java.util.Random;
 
 public class uzd3 {
     public static void main(String[] args) {
-    
+        int[] arr = new int[10];
+        Random rand = new Random();
+
+        // Aizpilda masīvu ar nejaušiem skaitļiem no 1 līdz 100
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100) + 1;
+        }
+        
+        int max = arr[0];
+        int min = arr[0];
+
+        // Atrod max un min
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        // Izvada Max un Min
+        System.out.println("\nMax: " + max);
+        System.out.println("Min: " + min);
+
+        // Izvada masīva elementus apgrieztā secībā
+        System.out.println("Masīvs apgrieztā secībā:");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
