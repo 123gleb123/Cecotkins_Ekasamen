136962ceb9c61720c5808813d1f74f3e9e1b6824
123gleb123 <gchechotkin@gmail.com>
2025-06-11 06:51:35 +0300

    pabeidzu taisit 1.uzd

--- src/uzd1.java
+++ src/uzd1.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class uzd1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[ ] args) {
+    	int[] numbers = new int[5];
+    	Scanner scan = new Scanner(System.in);
+    	
+    	//Ievade
+    	for(int i=0; i<numbers.length;i++) {
+    		System.out.println("Ievadiet " + (i + 1) + ". skaitli: ");
+    		numbers[i] = scan.nextInt();
+    	}
+    	scan.close();
+    	
+    	//Izvade
+    	System.out.println("Jūsu ievadītie skaitļi:");
+    	for(int num : numbers) {
+    		System.out.print(num+" ");
+    	}
+    }
 }
