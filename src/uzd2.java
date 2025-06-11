c870d49525517b358c113f583e6f0acf22c9265f
123gleb123 <gchechotkin@gmail.com>
2025-06-11 09:27:16 +0300

    pabeidzu taisit 2.uzd

--- src/uzd2.java
+++ src/uzd2.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class uzd2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] numbers = new int[7];
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        
+      //Ievade
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Ievadiet skaitli " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+            sum += numbers[i];
+        }
+        
+        // Aprēķina aritmētisko vidējo
+        double average = (double) sum / numbers.length;
+        
+     // Izvada rezultātus
+        System.out.println("Summa: " + sum);
+        System.out.println("Viedejais artmētiskais: " + average);
 
 	}
-
 }
